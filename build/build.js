214b055b65ceebbdff1d77eaee81a7ed49db8011
Hsiaoming Yang <me@lepture.com>
2014-03-26 13:43:42 +0800

    Update documentation

---
+++ build/build.js
@@ -0,0 +1,369 @@
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module._resolving && !module.exports) {
+    var mod = {};
+    mod.exports = {};
+    mod.client = mod.component = true;
+    module._resolving = true;
+    module.call(this, mod.exports, require.relative(resolved), mod);
+    delete module._resolving;
+    module.exports = mod.exports;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("component-event/index.js", function(exports, require, module){
+var bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
+    unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
+    prefix = bind !== 'addEventListener' ? 'on' : '';
+
+/**
+ * Bind `el` event `type` to `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.bind = function(el, type, fn, capture){
+  el[bind](prefix + type, fn, capture || false);
+  return fn;
+};
+
+/**
+ * Unbind `el` event `type`'s callback `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.unbind = function(el, type, fn, capture){
+  el[unbind](prefix + type, fn, capture || false);
+  return fn;
+};
+});
+require.register("component-query/index.js", function(exports, require, module){
+function one(selector, el) {
+  return el.querySelector(selector);
+}
+
+exports = module.exports = function(selector, el){
+  el = el || document;
+  return one(selector, el);
+};
+
+exports.all = function(selector, el){
+  el = el || document;
+  return el.querySelectorAll(selector);
+};
+
+exports.engine = function(obj){
+  if (!obj.one) throw new Error('.one callback required');
+  if (!obj.all) throw new Error('.all callback required');
+  one = obj.one;
+  exports.all = obj.all;
+  return exports;
+};
+
+});
+require.register("notice/index.js", function(exports, require, module){
+/**
+ * Notice
+ *
+ * A notice message at the top of a webpage.
+ *
+ * Copyright (c) 2014 by Hsiaoming Yang.
+ */
+
+var query = require('query');
+var events = require('event');
+
+var _COUNT = 0;
+
+function Notice(options) {
+  var el = createElement(options);
+  el.id = 'notice-' + (_COUNT++);
+  this.el = el;
+}
+
+Notice.prototype.show = function() {
+  if (document.getElementById(this.el.id)) return;
+
+  var container = query('.notice-container');
+  if (!container) {
+    container = document.createElement('div');
+    container.className = 'notice-container';
+    document.body.appendChild(container);
+  }
+  container.appendChild(this.el);
+};
+
+Notice.prototype.clear = function() {
+  dismiss(this.el);
+};
+
+function createElement(options) {
+  // div.notice-item
+  //   span.notice-close
+  //   div.notice-content
+  var container = document.createElement('div');
+  container.className = 'notice-item';
+  if (options.type) {
+    container.className += ' ' + options.type;
+  }
+
+  var content;
+  if (options.url) {
+    content = document.createElement('a');
+    content.href = options.url;
+    content.target = '_blank';
+  } else {
+    content = document.createElement('div');
+  }
+  content.className = 'notice-content';
+  content.innerHTML = options.message;
+
+  var close = document.createElement('span');
+  close.className = 'notice-close';
+  close.innerHTML = '×';
+
+  container.appendChild(close);
+  container.appendChild(content);
+
+  events.bind(close, 'click', function(e) {
+    dismiss(container);
+  });
+  return container;
+}
+
+function dismiss(el) {
+  el.className += ' notice-dismiss';
+  setTimeout(function() {
+    if (el && el.parentNode) {
+      el.parentNode.removeChild(el);
+    }
+  }, 200);
+}
+
+function notify(options, cb) {
+  if (!options) return;
+  if (!options.message) {
+    options = {message: options};
+  }
+  var time = options.duration || 4000;
+  var item = new Notice(options);
+  item.show();
+  setTimeout(function() {
+    item.clear();
+    cb && cb();
+  }, time);
+}
+notify.Notice = Notice;
+module.exports = notify;
+
+});
+
+
+
+
+require.alias("component-event/index.js", "notice/deps/event/index.js");
+require.alias("component-event/index.js", "event/index.js");
+
+require.alias("component-query/index.js", "notice/deps/query/index.js");
+require.alias("component-query/index.js", "query/index.js");
+
+require.alias("notice/index.js", "notice/index.js");
